cb33875c1b2570581062a5409acf64e679ea989d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-05-17 14:53:02 +0200

    chore: create database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+focus_group_die = Die.create! title: "Zielgruppe"
+
+FOCUS_GROUPS = ["Verwaltung", "Millenials", "Gen Z", "Politiker", "Alleinerziehende", "Hobbygärtner"]
+
+FOCUS_GROUPS.each do |focus_group|
+  Side.create! die: focus_group_die, title: focus_group
+end
+
+topic_die = Die.create! title: "Zielgruppe"
+
+TOPICS = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Workshop", "Wasser"]
+
+TOPICS.each do |topic|
+  Side.create! die: topic_die, title: topic
+end
+
+medium_die = Die.create! title: "Zielgruppe"
+
+MEDIA = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Workshop", "Wasser"]
+
+MEDIA.each do |medium|
+  Side.create! die: medium_die, title: medium
+end
