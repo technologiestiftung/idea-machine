db871dae245090d977e052ce1723e774c51b1279
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-05-17 16:20:55 +0200

    feat: display latest rolls
    Not yet live updating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @latest_rolls = []
+    Die.all.each do |die|
+      @latest_rolls << Roll.latest_for(die: die)
+    end
   end
 end
--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,3 +1,7 @@
 class Roll < ApplicationRecord
   belongs_to :side
+
+  def self.latest_for(die:)
+    Roll.where(side: die.sides).order("created_at DESC").first
+  end
 end
