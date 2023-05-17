4f8528c1f2edf534207b2c5c51672c9daad15915
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-05-17 16:50:40 +0200

    feat: broadcast latest roll to page

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,7 +1,17 @@
 class Roll < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :side
+
+  after_create_commit :update_latest_roll
 
   def self.latest_for(die:)
     Roll.where(side: die.sides).order("created_at DESC").first
+  end
+
+  private
+
+  def update_latest_roll
+    broadcast_update_to(dom_id(side.die), target: dom_id(side.die), partial: "sides/side", locals: {side: side})
   end
 end
