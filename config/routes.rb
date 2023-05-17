ff0dcf9987472b236c32b4a1aad9519cb7807d05
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-05-17 14:55:50 +0200

    feat: generate homepage
    That is currently empty

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :rolls, only: [:create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rolls, only: [:create]
 end
