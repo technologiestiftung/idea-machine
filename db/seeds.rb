1f66cd5fb4d14d3681484554a6f39e0ceddf86ab
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-05-17 16:20:19 +0200

    fix: use correct seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@ FOCUS_GROUPS.each do |focus_group|
   Side.create! die: focus_group_die, title: focus_group
 end
 
-topic_die = Die.create! title: "Zielgruppe"
+topic_die = Die.create! title: "Thema"
 
-TOPICS = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Workshop", "Wasser"]
+TOPICS = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Müll", "Wasser"]
 
 TOPICS.each do |topic|
   Side.create! die: topic_die, title: topic
 end
 
-medium_die = Die.create! title: "Zielgruppe"
+medium_die = Die.create! title: "Medium"
 
-MEDIA = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Workshop", "Wasser"]
+MEDIA = ["App", "Website", "Chatbot", "API", "Workshop", "Prototyp"]
 
 MEDIA.each do |medium|
   Side.create! die: medium_die, title: medium
