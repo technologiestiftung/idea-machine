eb50fa1f3e8c3b3e3794c937372a05baa3583b2a
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-01 17:27:32 +0200

    chore: allow ngrok tunneling for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow to serve localhost via ngrok:
+  config.hosts << /[a-z0-9-]+\.ngrok-free\.app/
 end
