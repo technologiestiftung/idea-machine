9631b7b8ea15058fc860c38a94e5fe0a2ed53e5d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-05 13:22:06 +0200

    chore: bring back idea generation

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,33 +4,33 @@ class IdeasController < ApplicationController
   end
 
   def create
-    # rolls = Roll.where(id: idea_params[:roll_ids])
-    # topics = Side.where(id: rolls.pluck(:side_id)).pluck(:title)
+    rolls = Roll.where(id: idea_params[:roll_ids])
+    topics = Side.where(id: rolls.pluck(:side_id)).pluck(:title)
 
-    # openai_client = OpenAI::Client.new(access_token: Rails.application.credentials.openai_token)
+    openai_client = OpenAI::Client.new(access_token: Rails.application.credentials.openai_token)
 
-    # response = openai_client.chat(
-    #   parameters: {
-    #     model: "gpt-3.5-turbo",
-    #     messages: [{role: "user", content: "Generiere eine lustige Idee für ein innovatives Projekt in der Verwaltunsdigitalisierung. Die Idee soll enthalten: #{topics}. Die Idee soll in einem Satz zusammengefasst werden."}],
-    #     temperature: 0.7
-    #   }
-    # )
+    response = openai_client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: "Generiere eine lustige Idee für ein innovatives Projekt in der Verwaltunsdigitalisierung. Die Idee soll enthalten: #{topics}. Die Idee soll in einem Satz zusammengefasst werden."}],
+        temperature: 0.7
+      }
+    )
 
-    # generated_idea = response.dig("choices", 0, "message", "content")
+    generated_idea = response.dig("choices", 0, "message", "content")
 
-    # @idea = Idea.new(description: generated_idea)
+    @idea = Idea.new(description: generated_idea)
 
-    # respond_to do |format|
-    #   if @idea.save
-    #     format.html do
-    #       redirect_to idea_url(@idea),
-    #         notice: "Idea was created"
-    #     end
-    #   else
-    #     # format.json { render json: @roll.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @idea.save
+        format.html do
+          redirect_to idea_url(@idea),
+            notice: "Idea was created"
+        end
+      else
+        # format.json { render json: @roll.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
