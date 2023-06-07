014df2016816b422438b03fbf248e874832e16de
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-07 14:35:02 +0200

    test: authorize roll POSTs

--- test/controllers/api/v1/rolls_controller_test.rb
+++ test/controllers/api/v1/rolls_controller_test.rb
@@ -1,25 +1,26 @@
 require "test_helper"
 
-class RollsControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::RollsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @roll = rolls(:one)
     @valid_shortcode = "#{dice(:one).shortcode}#{sides(:one).shortcode}"
     @invalid_shortcode = "#{dice(:one).shortcode}#{sides(:two).shortcode}" # Invalid because side does not belong to die
+    @authorization = "Bearer #{Rails.application.credentials.http_token}"
   end
 
   test "creates roll" do
     assert_difference("Roll.count") do
-      post rolls_url, params: {roll: {shortcode: @valid_shortcode}}, as: :json
+      post api_v1_rolls_url, params: {shortcode: @valid_shortcode}, headers: {Authorization: @authorization}, as: :json
     end
   end
 
   test "responds with status code :created after creation" do
-    post rolls_url, params: {roll: {shortcode: @valid_shortcode}}, as: :json
+    post api_v1_rolls_url, params: {shortcode: @valid_shortcode}, headers: {Authorization: @authorization}, as: :json
     assert_response :created
   end
 
   test "responds with status code :unprocessable_entity after unprocessable creation attempt" do
-    post rolls_url, params: {roll: {shortcode: @invalid_shortcode}}, as: :json
+    post api_v1_rolls_url, params: {shortcode: @invalid_shortcode}, headers: {Authorization: @authorization}, as: :json
     assert_response :unprocessable_entity
   end
 end
