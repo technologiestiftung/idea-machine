92bf82e7a6200464c12da57c1f03bd29a487a1bc
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-15 10:33:56 +0200

    fix: only render dice that have rolls
    Previously we had some nil's in the @latest_rolls. This caused the UI to crash because we tried accessing methods on nil's. This change only adds rolls to the @latest_rolls if a roll is found.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
     @latest_rolls = []
     Die.all.each do |die|
-      @latest_rolls << Roll.latest_for(die: die)
+      latest_roll = Roll.latest_for(die: die)
+      @latest_rolls << latest_roll unless latest_roll.blank?
     end
   end
 end
