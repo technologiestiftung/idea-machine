1916b466bdaab502d00d89aae92b29487136f903
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-15 10:50:25 +0200

    feat: change seeds to actual content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 focus_group_die = Die.create! title: "Zielgruppe", shortcode: "A"
 
-FOCUS_GROUPS = ["Verwaltung", "Millenials", "Gen Z", "Politiker", "Alleinerziehende", "Hobbygärtner"]
+FOCUS_GROUPS = ["Berliner Bürger:innen", "Kinder", "Verwaltungs-Angestellte", "Senior:innen", "Familien", "Nachtschwärmer"]
 
 FOCUS_GROUPS.each_with_index do |focus_group, idx|
   Side.create! die: focus_group_die, title: focus_group, shortcode: idx + 1
@@ -8,7 +8,7 @@ end
 
 topic_die = Die.create! title: "Thema", shortcode: "B"
 
-TOPICS = ["Bäume", "Parks", "Fahrräder", "Straßenbahn", "Müll", "Wasser"]
+TOPICS = ["Soziales Miteinander", "Mobilität", "Gesundheit", "Stadtentwicklung", "Wohnen", "Energie und Umwelt"]
 
 TOPICS.each_with_index do |topic, idx|
   Side.create! die: topic_die, title: topic, shortcode: idx + 1
@@ -16,7 +16,7 @@ end
 
 medium_die = Die.create! title: "Medium", shortcode: "C"
 
-MEDIA = ["App", "Website", "Chatbot", "API", "Workshop", "Prototyp"]
+MEDIA = ["Web-App", "mobile App", "API", "Künstliche Intelligenz", "Virtual Reality", "IoT"]
 
 MEDIA.each_with_index do |medium, idx|
   Side.create! die: medium_die, title: medium, shortcode: idx + 1
