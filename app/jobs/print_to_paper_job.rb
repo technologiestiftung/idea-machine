1d68a397e1f6be33201e179f5bd57b0c37318207
Raphael Arce <raphael.arce@ts.berlin>
2023-06-19 12:45:52 +0200

    feat: added print command
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- app/jobs/print_to_paper_job.rb
+++ app/jobs/print_to_paper_job.rb
@@ -4,8 +4,7 @@ class PrintToPaperJob < ApplicationJob
   def perform(text)
     Net::SSH.start(Rails.application.credentials.printing_raspi["hostname"], Rails.application.credentials.printing_raspi["username"], password: Rails.application.credentials.printing_raspi["password"]) do |ssh|
       # Print command here
-      pwd = ssh.exec!("pwd")
-      puts "Tunnelled into #{pwd}"
+      ssh.exec!("echo '#{text}' | lp -d POS-58-Series")
       ssh.exec!("exit\n")
     end
   end
