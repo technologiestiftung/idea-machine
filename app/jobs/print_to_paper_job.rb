afd660b685add2cb1d465b0914e9dc39bd0fbd90
Raphael Arce <raphael.arce@ts.berlin>
2023-06-21 14:15:10 +0200

    feat: improved print command
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- app/jobs/print_to_paper_job.rb
+++ app/jobs/print_to_paper_job.rb
@@ -3,7 +3,15 @@ class PrintToPaperJob < ApplicationJob
 
   def perform(text)
     Net::SSH.start(Rails.application.credentials.printing_raspi["hostname"], Rails.application.credentials.printing_raspi["username"], password: Rails.application.credentials.printing_raspi["password"]) do |ssh|
-      ssh.exec!("echo '#{text}' | lp -d POS-58-Series")
+
+      command = <<-TEXT
+echo 'Betreff: Idee
+Von: Ideenwürfel
+------------------
+#{text}' | fold -w 18 -s | lp -d POS-58-Series
+TEXT
+
+      ssh.exec!(command)
       ssh.exec!("exit\n")
     end
   end
