494e4301333714a648deeaeb26a148c5be7fd822
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-22 12:19:02 +0200

    style: update color theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
@@ -6,38 +7,20 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/components/**/*",
   ],
   theme: {
     colors: {
-      magenta: {
-        50: "#feedf1",
-        100: "#fddbe3",
-        200: "#fbb7c7",
-        300: "#fa94aa",
-        400: "#f8708e",
-        500: "#f64c72",
+      pink: {
+        DEFAULT: "#DE73C5",
       },
       blue: {
-        50: "#eaeaf6",
-        100: "#d5d5ec",
-        200: "#acacda",
-        300: "#8282c7",
-        400: "#5959b5",
-        500: "#2f2fa2",
+        DEFAULT: "#0043C0",
       },
-      gray: {
-        25: "#f5f5f7",
-        50: "#ebebef",
-        100: "#d7d8df",
-        200: "#b0b0bf",
-        300: "#8889a0",
-        400: "#616180",
-        500: "#393a60",
-        600: "#2e2e4d",
-        700: "#22233a",
-        800: "#171726",
-        900: "#0b0c13",
+      yellow: {
+        DEFAULT: "#E9D502",
       },
+      gray: colors.gray,
       white: "#fff",
     },
     extend: {
