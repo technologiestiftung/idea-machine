02773009b8a2054c5215c5b98a9e35844ff5df03
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-06-27 14:38:15 +0200

    feat: prepare to seed db with new side items

--- app/models/side.rb
+++ app/models/side.rb
@@ -1,7 +1,12 @@
 class Side < ApplicationRecord
+  serialize :variations
+  after_initialize do |side|
+    side.variations ||= [side.title] if side.variations.blank?
+  end
+
   belongs_to :die
   has_many :rolls, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, :variations, presence: true
   validates :shortcode, presence: true, uniqueness: {scope: :die_id}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,28 @@
 focus_group_die = Die.create! title: "focus_group", shortcode: "A"
 
-FOCUS_GROUPS = ["Berliner Bürger:innen", "Kinder", "Verwaltungs-Angestellte", "Senior:innen", "Familien", "Nachtschwärmer"]
+FOCUS_GROUPS = [
+  {title: "Berliner Bürger:innen"},
+  {title: "Unternehmen"},
+  {title: "Verwaltung", variations: ["Mitarbeitende der Berliner Verwaltung"]},
+  {title: "Familien"},
+  {title: "Junge Menschen"},
+  {title: "?", variations: ["Senior:innen", "Benachteiligte Menschen", "Verliebte", "Füchse", "Such dir eine aus"]}
+]
 
 FOCUS_GROUPS.each_with_index do |focus_group, idx|
-  Side.create! die: focus_group_die, title: focus_group, shortcode: idx + 1
+  Side.create! die: focus_group_die, title: focus_group[:title], variations: focus_group[:variations], shortcode: idx + 1
 end
 
 topic_die = Die.create! title: "topic", shortcode: "B"
 
-TOPICS = ["Soziales Miteinander", "Mobilität", "Gesundheit", "Stadtentwicklung", "Wohnen", "Energie und Umwelt"]
+TOPICS = [
+  {title: "Soziales Miteinander"},
+  {title: "Kultur & Freizeit"},
+  {title: "Wohnen & Energie"},
+  {title: "Bildung"},
+  {title: "Mobilität & Verkehr"},
+  {title: "?", variations: ["Gesundheit", "Ernährung", "Umwelt", "Nachhaltigkeit", "Such dir eins aus"]}
+]
 
 TOPICS.each_with_index do |topic, idx|
   Side.create! die: topic_die, title: topic, shortcode: idx + 1
@@ -16,7 +30,14 @@ end
 
 medium_die = Die.create! title: "medium", shortcode: "C"
 
-MEDIA = ["Web-App", "mobile App", "API", "Künstliche Intelligenz", "Virtual Reality", "IoT"]
+MEDIA = [
+  {title: "Website & App"},
+  {title: "Öffentlicher Raum"},
+  {title: "Social Media Kanal"},
+  {title: "Virtual & Augmented Reality"},
+  {title: "IoT"},
+  {title: "?", variations: ["Kunstinstallation", "Theaterstück", "Oper", "Petition", "Such dir eins aus"]}
+]
 
 MEDIA.each_with_index do |medium, idx|
   Side.create! die: medium_die, title: medium, shortcode: idx + 1
