2ccdb14aeeb644c6344c0b669a2308cdf4329f97
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-08-03 12:13:35 +0200

    feat: create initial roll on side creation
    This mostly makes sure that we have @latest_rolls for each die, so that ide creation could be triggered immediately.

--- app/models/side.rb
+++ app/models/side.rb
@@ -3,10 +3,17 @@ class Side < ApplicationRecord
   after_initialize do |side|
     side.variations ||= [side.title] if side.variations.blank?
   end
+  after_commit :roll_side, on: [:create], if: proc { |side| side.die.rolls.empty? }
 
   belongs_to :die
   has_many :rolls, dependent: :destroy
 
   validates :title, :variations, presence: true
   validates :shortcode, presence: true, uniqueness: {scope: :die_id}
+
+  private
+
+  def roll_side
+    Roll.create side: self
+  end
 end
