b6bf472520411acb4e6d6e1f032e1f0d91e7eb10
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-09-29 13:16:27 +0200

    fix: messed up indexes
    I know, it's dirty to change a migration subsequently. However, we need to deploy this now and the codebase does not have a future anyways, so let's accept this little not-so-best-practice.

--- db/migrate/20230805105911_make_game_required_for_die.rb
+++ db/migrate/20230805105911_make_game_required_for_die.rb
@@ -1,8 +1,8 @@
 class MakeGameRequiredForDie < ActiveRecord::Migration[7.0]
   def change
     change_column_null :dice, :game_id, false
-    remove_index :dice, :shortcode
-    remove_index :dice, :game_id
-    add_index :dice, [:shortcode, :game_id], unique: true
+    remove_index :dice, :shortcode, if_exists: true
+    remove_index :dice, :game_id, if_exists: true
+    add_index :dice, [:shortcode, :game_id], unique: true, if_not_exists: true
   end
 end
